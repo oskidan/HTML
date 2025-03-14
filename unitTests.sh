45ad6e073edb71815889ee28bc52456a21e12c88
Oleksii Skidan <al.skidan@gmail.com>
2025-03-14 11:48:45 +0100

    build: Added passthrough arguments to carton test

--- unitTests.sh
+++ unitTests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # You can add `--verbose` flag to get more output if anything goes wrong.
-swift run carton test --headless --environment browser
+swift run carton test --headless --environment browser $*
